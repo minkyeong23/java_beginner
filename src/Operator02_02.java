374785bf5f4af1750ceffe66a05f5c273f711bb0
minkyeong23 <choi777765@gmail.com>
2026-03-31 21:53:14 +0900

    삼항 연산자 공부

---
+++ src/Operator02_02.java
@@ -0,0 +1,21 @@
+public class Operator02_02 {
+    public static void main(String[] args) {
+
+//        삼항 연산자 : 이거 맞냐 묻는 `?` 조건이 참일 때 값 : 거짓일 때 값
+//        조건 (괄호)로 묶는게 읽기 편함
+        int num1 = 1;
+        int num2 = 2;
+        int age = 26;
+
+//        나이 판별: age가 20 이상이면 "성인", 아니면 "미성년자"를 변수에 저장
+        String resultStr = (age >= 20) ? "성인" : "미성년자";
+
+//       숫자 비교: num1이 num2보다 크면 num1, 아니면 num2를 선택
+        int result = (num1 > num2) ? num1 : num2;
+
+        System.out.println(result); // 2
+        System.out.println(resultStr); // 성인
+
+        System.out.println((15 >= 20) ? "성인" : "미성년자"); // 직접 값을 넣어 거짓 케이스 확인
+    }
+}
