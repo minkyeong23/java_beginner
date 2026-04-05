fff10c9a37da5bb57f95b5c7c5dcc96e8915757e
minkyeong23 <choi777765@gmail.com>
2026-04-05 21:31:24 +0900

    feat: (반복문) for 공부 + 주석 추가

--- src/_05For.java
+++ src/_05For.java
@@ -1,10 +1,13 @@
 public class _05For {
     public static void main(String[] args) {
         int sum =  0;
+/**       for문 for(초기값; 조건식; 증감연산자) { 반복할 내용 }
+ *        1. 시작 숫자 정하기 i = 1; 2. 언제까지 할지 정하기 i <= 9; 3. 하나씩 키우기 또는 줄이기 i++, i--
+ */
 
 //        구구단 2단 출력
         for (int i = 1; i <= 9; i++) {
-            System.out.println("2*" + i + "=" + (2 * i));
+            System.out.println(" 2 * " + i + " = " + (2 * i));
         }
 //        1부터 10까지 더하기
         for (int j =  1; j <= 10; j++) {
